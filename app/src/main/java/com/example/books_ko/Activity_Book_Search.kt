3558f32eabd7c5c321d366a5a0e872dbab3ade45
superphs333 <superphs333@naver.com>
2023-05-06 11:18:46 +0900

    가져온 도서 리사이클러뷰 셋팅_코드최적화

--- app/src/main/java/com/example/books_ko/Activity_Book_Search.kt
+++ app/src/main/java/com/example/books_ko/Activity_Book_Search.kt
@@ -1,6 +1,5 @@
 package com.example.books_ko
 
-
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -19,34 +18,29 @@ import org.json.JSONObject
 class Activity_Book_Search : AppCompatActivity() {
 
     private lateinit var binding: ActivityBookSearchBinding
-
-    // 도서
-    var arrayList: ArrayList<Data_Search_Book>? = null
-        // 이렇게 초기화를 해주어야 arrayList에 객체가 담겨짐
-
-    /*
-    리사이클러뷰
-     */
-    var  mainAdapter: AdapterSearchBook? = null
-    lateinit var linearLayoutManager:LinearLayoutManager
-
-
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: AdapterSearchBook
+    private var bookList: ArrayList<Data_Search_Book> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookSearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        arrayList = ArrayList();
+        linearLayoutManager = LinearLayoutManager(this)
+        adapter = AdapterSearchBook(bookList, applicationContext, this)
 
-
+        binding.rvBooks.apply {
+            setHasFixedSize(true)
+            layoutManager = linearLayoutManager
+            adapter = this@Activity_Book_Search.adapter
+        }
     }
 
-    open fun search_book(view: View?){
-        // arrayList초기화
-        arrayList?.clear()
+    fun search_book(view: View?) {
+        bookList.clear()
+        adapter.notifyDataSetChanged()
 
-        val url =
-            "https://dapi.kakao.com" + "/v3/search/book?query=" + binding.editSearch.text.toString()
+        val url = "https://dapi.kakao.com" + "/v3/search/book?query=" + binding.editSearch.text.toString()
         val request: StringRequest = object : StringRequest(
             Method.GET,
             url,
@@ -56,48 +50,28 @@ class Activity_Book_Search : AppCompatActivity() {
                 Log.i("정보태그", "(search_book)response=>$response")
 
                 val jsonObject = JSONObject(response)
-                val TEMP = jsonObject.getJSONArray("documents")
+                val documents = jsonObject.getJSONArray("documents")
 
-                for (i in 0 until TEMP.length()) {
-                    val tempDataSearchBook = Data_Search_Book()
-
-                    val temp_object = TEMP.getJSONObject(i)
-                    //Log.i("정보태그",temp_object.toString())
+                for (i in 0 until documents.length()) {
+                    val tempObject = documents.getJSONObject(i)
 
                     /*
                     가져 올 정보 : 작가, contents, isbn
                      */
-                    // 작가
-                    tempDataSearchBook.authors =
-                        temp_object.getString("authors").replace("\\[".toRegex(), "")
+                    val authors =
+                        tempObject.getString("authors").replace("\\[".toRegex(), "")
                             .replace("\\]".toRegex(), "").replace("\"".toRegex(), "")
-                    // contents
-                    tempDataSearchBook.contents = temp_object.getString("contents")
-                    // isbn
-                    val string_array = temp_object.getString("isbn").split(" ".toRegex())
-                        .dropLastWhile { it.isEmpty() }
-                        .toTypedArray()
-                    tempDataSearchBook.isbn = if (string_array[0].isBlank()) string_array[1] else string_array[0]  // 만약, 10자리 isbn이 없다면 13자리 isbn사용
-                    // 그외 정보
-                    tempDataSearchBook.publisher = temp_object.getString("publisher")
-                    tempDataSearchBook.thumbnail = temp_object.getString("thumbnail")
-                    tempDataSearchBook.title = temp_object.getString("title")
-                    tempDataSearchBook.url = temp_object.getString("url")
+                    val contents = tempObject.getString("contents")
+                    val isbn = if (tempObject.getString("isbn").length == 10) tempObject.getString("isbn") else tempObject.getString("isbn")
+                    val publisher = tempObject.getString("publisher")
+                    val thumbnail = tempObject.getString("thumbnail")
+                    val title = tempObject.getString("title")
+                    val url = tempObject.getString("url")
 
-                    // 리스트에 객체 넣기
-                    arrayList?.add(tempDataSearchBook)
-                    Log.i("정보태그", "tempDataSearchBook->$tempDataSearchBook")
-
+                    bookList.add(Data_Search_Book(isbn, title, authors, publisher, thumbnail, contents, "Kakao", url))
                 }
-                Log.i("정보태그", "도서 담기 완료, arrayList->$arrayList")
-                // 리사이클러뷰에 셋팅
-                mainAdapter = AdapterSearchBook(arrayList!!,applicationContext,this@Activity_Book_Search)
-                binding.rvBooks.adapter = mainAdapter
-                linearLayoutManager = LinearLayoutManager(applicationContext)
-                linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
-                binding.rvBooks.layoutManager = linearLayoutManager
-
-
+                Log.i("정보태그", "도서 담기 완료, arrayList->$bookList")
+                adapter.notifyDataSetChanged()
 
             }, // end onResponse
             Response.ErrorListener { error ->
