1109ac2b2a8c2b4e157a5a184e4544476e28a1ba
superphs333 <superphs333@naver.com>
2023-05-13 21:58:21 +0900

    채팅방에서 팔로우 기능

--- app/src/main/java/com/example/books_ko/Adapter/Adapter_Join_People.kt
+++ app/src/main/java/com/example/books_ko/Adapter/Adapter_Join_People.kt
@@ -1,30 +1,23 @@
 package com.example.books_ko.Adapter
 
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.ContentResolver
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.books_ko.Activity_Add_Chatting_Room
-import com.example.books_ko.Data.Data_Chatting_Room
 import com.example.books_ko.Data.Data_Join_People
 import com.example.books_ko.Function.FunctionCollection
 import com.example.books_ko.R
-import com.example.books_ko.databinding.ItemChattingRoomBinding
 import com.example.books_ko.databinding.ItemJoinPeopleBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import okhttp3.internal.notify
 
 class Adapter_Join_People (
     var dataList: ArrayList<Data_Join_People> = ArrayList(),
@@ -81,6 +74,23 @@ class Adapter_Join_People (
         binding.txtRole.visibility = if (item.email == leader) View.VISIBLE else View.GONE
 
 
+        /*
+        팔로우 기능
+         */
+        binding.txtFollow.setOnClickListener{
+            val map: MutableMap<String, String> = HashMap()
+            map["from_email"] = email
+            map["from_nickname"] = item.nickname // fcm알람용
+            map["to_email"] = item.email
+            CoroutineScope(Dispatchers.Main).launch  {
+                val goServer = fc.goServer(context,"following",map)
+                if(goServer){
+                    binding.txtFollow.visibility = View.GONE
+                    item.follow = true
+                    notifyDataSetChanged()
+                }
+            }
+        }
 
 
 
