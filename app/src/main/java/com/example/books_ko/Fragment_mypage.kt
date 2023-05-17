45457942ede7e819d15078d3c27f1b4bdb2a355a
superphs333 <superphs333@naver.com>
2023-05-17 20:32:15 +0900

    프래그먼트 마이페이지에 닉네임, 프로필 이미지 셋팅

--- app/src/main/java/com/example/books_ko/Fragment_mypage.kt
+++ app/src/main/java/com/example/books_ko/Fragment_mypage.kt
@@ -4,12 +4,22 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.example.books_ko.DataBase.UserDatabase
 import com.example.books_ko.Function.AboutMember
 import com.example.books_ko.databinding.FragmentMypageBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class Fragment_mypage : Fragment() {
@@ -18,6 +28,7 @@ class Fragment_mypage : Fragment() {
     private lateinit var activity: Activity
     val am = AboutMember
 
+    private lateinit var database : UserDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +45,41 @@ class Fragment_mypage : Fragment() {
 
         activity = requireActivity()
         context = requireContext()
+
+        /*
+        내용 셋팅
+         */
+        // 데이터 불러오기
+        database = Room.databaseBuilder(context, UserDatabase::class.java, "app_database").build()
+        val userLiveData = database.userDao().getUser()
+        CoroutineScope(Dispatchers.IO).launch{
+            withContext(Dispatchers.Main){
+                userLiveData.observe(activity as FragmentActivity, Observer { userLiveData ->
+                    email = userLiveData.email
+                    Log.i("정보태그","email->$email")
+                    CoroutineScope(Dispatchers.IO).launch{
+                        val image_Uri = AboutMember.getMemberInfo(context,email,"profile_url")
+                        val nickname = AboutMember.getMemberInfo(context,email,"nickname")
+                        if(image_Uri!=null || image_Uri!=""){
+                            withContext(Dispatchers.Main){
+                                // 프로필 이미지 셋팅
+                                val glideImg = if (image_Uri!!.contains(getString(R.string.img_profile))) {
+                                    getString(R.string.server_url).removeSuffix("/") + image_Uri
+                                } else image_Uri
+                                Log.i("정보태그","glideImg->$glideImg")
+                                Glide.with(context).load(glideImg).into(binding!!.imgProfile)
+                                // 닉네임
+                                binding!!.txtNickname.setText(nickname)
+                            }
+                        }
+
+
+
+                    }
+                })
+            }
+        }
+
 
         /*
         세팅 액티비티로 이동
